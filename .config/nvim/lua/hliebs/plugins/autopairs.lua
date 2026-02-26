9b054ca4b2058d26070c3ba2a9c87ed2b89114ee
HliebS <h.n.solomonov@gmail.com>
2026-02-26 21:41:32 +0100

    chore: adjust autopairs config

--- .config/nvim/lua/hliebs/plugins/autopairs.lua
+++ .config/nvim/lua/hliebs/plugins/autopairs.lua
@@ -1,16 +1,15 @@
 return {
-  "windwp/nvim-autopairs",
-  event = { "InsertEnter" },
-  config = function()
-    -- import nvim-autopairs
-    local autopairs = require("nvim-autopairs")
-    --
-    -- configure autopairs
-    autopairs.setup({
-      check_ts = true, -- enable treesitter
-      ts_config = {
-        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-      },
-    })
-  end,
+	"windwp/nvim-autopairs",
+	event = { "InsertEnter" },
+	config = function()
+		-- import nvim-autopairs
+		local autopairs = require("nvim-autopairs")
+		-- configure autopairs
+		autopairs.setup({
+			check_ts = true, -- enable treesitter
+			ts_config = {
+				lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+			},
+		})
+	end,
 }
