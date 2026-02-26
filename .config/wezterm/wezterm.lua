a80585c5d169dd84f8b4a08f7aa8f0d31834685e
HliebS <h.n.solomonov@gmail.com>
2026-02-26 21:41:50 +0100

    feat: specify graphical settings in wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,6 +2,10 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.front_end = "WebGpu"
+config.webgpu_power_preference = "HighPerformance"
+config.max_fps = 144
+
 config.font = wezterm.font 'JetBrains Mono'
 
 config.color_scheme = 'Tokyo Night'
